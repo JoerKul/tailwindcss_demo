4249d1ada1160859211bcce0274effd85b4932b6
Jörg Kullrich <joerg.kullrich@siemens.com>
2022-11-18 14:17:05 +0100

    inti & developed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,69 @@
-import Analytics from "./components/Analytics";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Newsletter from "./components/Newsletter";
-import Pricing from "./components/Pricing";
+import { Key } from "react";
+import { blogList } from "./blog-list.js";
+import { motion, Variants } from "framer-motion";
+import "./App.css";
+
+const imageAnimation: Variants = {
+  offscreen: { x: -100, opacity: 0 },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      bounce: 0.5,
+      duration: 1.5,
+    },
+  },
+};
+
+const textAnimation: Variants = {
+  offscreen: { y: -100, opacity: 0 },
+  onscreen: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      bounce: 0.5,
+      duration: 2.5,
+    },
+  },
+};
+
+function Card({
+  image,
+  h2,
+  p,
+  id,
+}: {
+  image: string;
+  h2: string;
+  p: string;
+  id: string;
+}) {
+  return (
+    <motion.div
+      className="card"
+      id={id}
+      initial={"offscreen"}
+      whileInView={"onscreen"}
+      transition={{ staggerChildren: 0.5 }}
+      viewport={{ once: false, amount: 0.5 }}
+    >
+      <motion.div className="image-container" variants={imageAnimation}>
+        {image}
+      </motion.div>
+      <motion.h2 variants={textAnimation}>{h2}</motion.h2>
+      <motion.p variants={textAnimation}>{p}</motion.p>
+    </motion.div>
+  );
+}
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Analytics />
-      <Newsletter />
-      <Pricing />
-      <Footer />
+  return blogList.map((blog, index) => (
+    <div className="card-wrapper" key={index}>
+      <Card {...blog} />
     </div>
-  );
+  ));
 }
 
 export default App;
---
+++ src/blog-list.js
@@ -0,0 +1,12 @@
+// array of objects
+export const blogList = [
+  { image: "🍅", h2: "Tomatos", p: "Yoshi finds tomatos" },
+  { image: "🍌", h2: "Bananas", p: "Yoshi finds bananas" },
+  { image: "🍎", h2: "Apples", p: "Yoshi finds apples" },
+  { image: "🍇", h2: "Grapes", p: "Yoshi finds grapes" },
+  { image: "🍑", h2: "Peaches", p: "Yoshi finds peaches" },
+  { image: "🍍", h2: "Pineapples", p: "Yoshi finds pineapples" },
+  { image: "🍉", h2: "Melons", p: "Yoshi finds watermelons" },
+  { image: "🍊", h2: "Oranges", p: "Yoshi finds oranges" },
+  { image: "🍋", h2: "Lemons", p: "Yoshi finds lemons" },
+];
